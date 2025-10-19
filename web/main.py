dd684c02f9407af78518eb6788604192a61ceb0d
doomka5 <doomka5@gmail.com>
2025-10-19 23:58:16 +0200

    Add LED module voltage settings

--- web/main.py
+++ web/main.py
@@ -147,6 +147,15 @@ async def startup():
     )
     await conn.execute(
         """
+        CREATE TABLE IF NOT EXISTS led_module_voltage_options (
+            id SERIAL PRIMARY KEY,
+            name TEXT UNIQUE NOT NULL,
+            created_at TIMESTAMPTZ DEFAULT timezone('utc', now())
+        )
+        """
+    )
+    await conn.execute(
+        """
         CREATE TABLE IF NOT EXISTS led_strip_manufacturers (
             id SERIAL PRIMARY KEY,
             name TEXT UNIQUE NOT NULL,
